f2478bb0bf40ef39fda671b11ceb28621909d5c7
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-11-27 07:47:23 -0500

    arreglo create

--- src/character/dto/create-character.dto.ts
+++ src/character/dto/create-character.dto.ts
@@ -1,6 +1,5 @@
 import { IsArray, IsBoolean, IsNumber, IsOptional, IsPositive, 
     IsString, MinLength } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger"
 import { Location } from "../entities/location.entity";
 
 export class CreateCharacterDto {
@@ -8,20 +7,16 @@ export class CreateCharacterDto {
     @IsString()
     name: string;
 
-    @ApiProperty()
     @IsPositive()
     @IsNumber()
     salary: number;
     
-    @ApiProperty()
     @IsBoolean()
     employee: boolean;
     
-    @ApiProperty()
     @IsOptional()
     property: Location;
 
-    @ApiProperty()
     @IsOptional()
     @IsArray()
     favPlaces?: Location[];
